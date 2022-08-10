a5a6e91ae3a08543fa41aaf496222a4bf111a7d6
HiImWhite <wojttasek1999@gmail.com>
2022-08-10 13:03:06 +0200

    Rolling number and Id's

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-'use strict'
+"use strict";
+
+const playerOne = document.querySelector(".player-one");
+const playerTwo = document.querySelector(".player-two");
+const scoreOne = document.getElementById("score--one");
+const scoreTwo = document.getElementById("score--two");
+
+const btnRoll = document.querySelector(".btn-roll");
+const btnHold = document.querySelector(".btn-hold");
+const btnRestart = document.querySelector(".btn-restart");
+
+let playerActive, playing, score;
+
+const init = function () {
+  playerActive = 0;
+  score = 0;
+  playing = true;
+};
+
+init();
+
+//Rolling number
+btnRoll.addEventListener("click", function () {
+  if (playing) {
+    const number = Math.trunc(Math.random() * 11) + 1;
+    score += number;
+    document.getElementById(`score--${playerActive}`).textContent = score;
+  }
+});
