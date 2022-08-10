42be9415920b757eb8930eb955568891689d4c6e
HiImWhite <wojttasek1999@gmail.com>
2022-08-10 14:54:11 +0200

    Added buttons transition and score array

--- script.js
+++ script.js
@@ -5,25 +5,37 @@ const playerTwo = document.querySelector(".player-two");
 const scoreOne = document.getElementById("score--one");
 const scoreTwo = document.getElementById("score--two");
 
-const btnRoll = document.querySelector(".btn-roll");
-const btnHold = document.querySelector(".btn-hold");
+const btnRoll = document.querySelector(".btn-roll-0");
+const btnHold = document.querySelector(".btn-hold-0");
 const btnRestart = document.querySelector(".btn-restart");
 
 let playerActive, playing, score;
 
 const init = function () {
   playerActive = 0;
-  score = 0;
+  score = [0, 0];
   playing = true;
 };
 
 init();
 
+const switchPlayer = function () {
+  document.getElementById(`player--${playerActive}`);
+  playerActive = playerActive === 0 ? 1 : 0;
+  playerOne.classList.toggle("player-active");
+  playerTwo.classList.toggle("player-active");
+  btnRoll.classList.toggle("btn-roll-1");
+  btnHold.classList.toggle("btn-hold-1");
+};
+
 //Rolling number
 btnRoll.addEventListener("click", function () {
   if (playing) {
     const number = Math.trunc(Math.random() * 11) + 1;
-    score += number;
-    document.getElementById(`score--${playerActive}`).textContent = score;
+    score[playerActive] += number;
+    document.getElementById(`score--${playerActive}`).textContent =
+      score[playerActive];
+
+    switchPlayer();
   }
 });
